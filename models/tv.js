3463afa7b39998900e78d386c23793ed9f257f6c
didietsuryadi <didietsuryadi>
2017-03-01 14:14:51 +0700

    create-route

--- models/tv.js
+++ models/tv.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose')
-var Schema = mongoose.schema()
+var Schema = mongoose.Schema
 
 var tvSchema = new Schema({
   title : String,
--- routes/api.js
+++ routes/api.js
@@ -3,17 +3,17 @@ var router = express.Router();
 var moviesController = require('../controllers/movie');
 var tvsController = require('../controllers/tv');
 
-router.get('/api/movies')
-router.get('/api/movies/:id')
-router.post('/api/movies')
-router.put('/api/movies/id')
-router.delete('/api/movies/:id')
+router.get('/api/movies', moviesController.readMovies)
+router.get('/api/movies/:id', moviesController.readMovie)
+router.post('/api/movies', moviesController.createMovie)
+router.put('/api/movies/id', moviesController.updateMovie)
+router.delete('/api/movies/:id', moviesController.deleteMovie)
 
-router.get('/api/tvs')
-router.get('/api/tvs/:id')
-router.post('/api/tvs')
-router.put('/api/tvs/id')
-router.delete('/api/tvs/:id')
+router.get('/api/tvs', tvsController.readTvs)
+router.get('/api/tvs/:id', tvsController.readTv)
+router.post('/api/tvs', tvsController.createTv)
+router.put('/api/tvs/id', tvsController.updateTv)
+router.delete('/api/tvs/:id', tvsController.deleteTv)
 
 
 module.exports = router;
